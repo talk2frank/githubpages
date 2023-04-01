4aa456d19e378fb0bb9366552be1d052d5adfb03
Frank Kinsey <frankin_c@hotmail.com>
2023-04-01 18:46:00 +0100

    add licensing

--- src/components/components.js
+++ src/components/components.js
@@ -222,7 +222,10 @@ export function ContactItem({link}){
 export function MyFooter(){
     return(
         <footer className='App-footer'>
-        <p className='AppFooter-tagline'>© 2023, Frank Kinsey</p>
+        <p className='AppFooter-tagline'>© 2023, Frank Kinsey. This work is licensed under a Creative Commons Attribution 4.0 International License&nbsp;</p>
+        <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
+          <img alt="Creative Commons License" src="https://i.creativecommons.org/l/by/4.0/88x31.png" />
+        </a>
       </footer>
     );
 };
