8c09706eb402cec68a642da72d75aace488a0fb9
Frank Kinsey <frankin_c@hotmail.com>
2023-04-06 14:47:54 +0100

    develop routing options react-router

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { themeToggle } from './components/components';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
